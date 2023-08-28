8e55e519b2955576186a0f5ed88e5c56125a9b40
mbessoni <michellebessoni@hotmail.com>
2023-08-27 21:58:01 -0300

    configurando IDE com node

---
+++ index.js
@@ -0,0 +1,10 @@
+const nota1 = 8;
+const nota2 = 7;
+const nota3 = 6;
+
+const media = (nota1 + nota2 + nota3) / 3;
+
+const situacao = media >= 7 ? "Aprovado" : "Reprovado";
+
+console.log(` Sua média é ${media} | Status: ${situacao}`);
+
